d376dcc6a51279021d18216858388a802fb4fe55
yevhenii.honcharenko <yevhenii.honcharenko@howly.com>
2023-10-07 16:56:50 +0300

    initial commit

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -1,10 +1,25 @@
+import {useSelector} from "react-redux";
+import {useMemo} from "react";
+
+const xStart = -4.8;
+const xStep = 1.1;
+
+const yStart = 3.4;
+const yStep = (3.4+5.4)/9;
 
 export const Player = ({ isSecondPlayer, playerName }) => {
-    const xPosition = !isSecondPlayer ? -0.4 : -0.4;
-    const zPosition = !isSecondPlayer ? 3.4 : -5.4;
+    const [x, y] = useSelector((state) => state.game[playerName]);
+
+    const xPosition = useMemo(() => {
+        return xStart + (xStep * (x));
+    }, []);
+
+    const yPosition = useMemo(() => {
+        return yStart - (yStep * (8 - y));
+    }, []);
     
     return (
-        <mesh position={[xPosition, 0.5 ,zPosition]}>
+        <mesh position={[xPosition, 0.5, yPosition]}>
             <boxGeometry attach="geometry" args={[1, 1, 1]} />
             <meshBasicMaterial attach="material" color={isSecondPlayer ? "green" :"red"} />
         </mesh>
